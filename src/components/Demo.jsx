79adc70f7d7cf5c3065c0db03a5dc820d58952ac
D_Zucho <danny.nico.dev@outlook.com>
2024-01-19 09:36:37 -0800

    add redux store and configurations

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,13 +2,21 @@ import { useState, useEffect } from 'react'
 import { copy, linkIcon, loader, tick } from '../assets'
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  })
+
+  const handleSubmit = async (e) => {
+    alert('Submitted!')
+  }
   return (
     <section className='mt-16 w-full max-w-xl'>
       {/* Search */}
       <div className='flex flex-col w-full gap-2'>
         <form
           className='relative flex justify-center items-center'
-          onSubmit={() => {}}
+          onSubmit={handleSubmit}
         >
           <img
             src={linkIcon}
@@ -18,8 +26,13 @@ const Demo = () => {
           <input
             type='url'
             placeholder='Enter url...'
-            value=''
-            onChange={() => {}}
+            value={article.url}
+            onChange={(e) =>
+              setArticle({
+                ...article,
+                url: e.target.value,
+              })
+            }
             className='url_input peer'
           />
           <button
